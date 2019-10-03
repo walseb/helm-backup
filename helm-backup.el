e6958dbfce343d6870fc00ca67d04515799ffbbd
Sebastian Wålinder <s.walinder@gmail.com>
2019-10-03 22:22:12 +0200

    Removed helm

--- helm-backup.el
+++ helm-backup.el
@@ -5,7 +5,7 @@
 ;; Author: Anthony HAMON <hamon.anth@gmail.com>
 ;; URL: http://github.com/antham/helm-backup
 ;; Version: 1.1.1
-;; Package-Requires: ((helm "1.5.5") (s "1.8.0") (cl-lib "0"))
+;; Package-Requires: ((s "1.8.0") (cl-lib "0"))
 ;; Keywords: backup, convenience, files, tools, vc
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -35,8 +35,6 @@
 
 ;;; Code:
 
-(require 'helm)
-(require 'helm-utils)
 (require 'cl-lib)
 (require 's)
 
